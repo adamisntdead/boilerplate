561f69da5afbc135fd79bfeee3bd53d85b274e5e
Adam Kelly <adam.guitar12@gmail.com>
2018-07-03 20:31:45 +0100

    Added Sass Gulp Task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 const gulp = require('gulp')
+const sass = require('gulp-sass')
+const watchSass = require('gulp-watch-sass')
 
-gulp.task('default', () => {
+const sassFiles = './src/scss/**/*.scss'
 
-})
+gulp.task('sass', () => gulp.src(sassFiles).pipe(sass()).pipe(gulp.dest('./dist/css')))
+
+gulp.task('sass:watch', () =>
+  watchSass(sassFiles)
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/css'))
+)
+
+// The development task, will initially compile things
+// and then watch for changes
+gulp.task('watch', ['sass', 'sass:watch'])
+
+// Build the production version of the assets
+gulp.task('default', ['sass'])
